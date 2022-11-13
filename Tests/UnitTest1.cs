92bf2459490eea8d924937e9b63137ca789ae0bd
Mohammad-Azim <mohammad.azim@optimumpartners.co>
2022-11-13 17:08:42 +0200

    add: testing Write first test

---
+++ Tests/UnitTest1.cs
@@ -0,0 +1,21 @@
+using System;
+using Xunit;
+using System.Text.Json;
+
+namespace Simulator
+{
+    public class FilesManagerTests
+    {
+
+
+
+        [Fact]
+        public void testStartReading()
+        {
+            FilesManager myFileManager = new FilesManager("../../../Tests/TestTasks.json");
+            Assert.Equal(2, myFileManager.cpuNumber);
+        }
+
+    }
+
+}
